78b1ec70f302c4388533e0c06b5b6ec73eab7c62
sathapornma <sathaporn.ma@rmuti.ac.th>
2020-10-06 21:35:48 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Firebase from 'firebase';
+
+function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDWVHPBz2f4xB0b2KHNKf--dcFjT_gbI3U",
+    authDomain: "saifah-react-realtime-database.firebaseapp.com",
+    databaseURL: "https://saifah-react-realtime-database.firebaseio.com",
+    projectId: "saifah-react-realtime-database",
+    storageBucket: "saifah-react-realtime-database.appspot.com",
+    messagingSenderId: "1023586764299",
+    appId: "1:1023586764299:web:937b49facc486d91b8a90e",
+    measurementId: "G-7QP33FDC5K"
+  };
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
